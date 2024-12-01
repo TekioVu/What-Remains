984bc1aa97af5b85d7957e4f79adf910365be890
Sergio Valiente <sevalien@ucm.es>
2024-12-01 19:56:54 +0100

    Cinemática 2.1 Cenando En la Mesa

--- Assets/HorrorMansion/Scripts/ChangeToScene3.cs
+++ Assets/HorrorMansion/Scripts/ChangeToScene3.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
 
 public class ChangeToScene3 : MonoBehaviour
 {
+    public PlayableDirector cutsceneToPlay;
+
     public void ChangeScene()
     {
         SceneManager.LoadScene("Cutscene_3");
+    }
+
+    public void ChangeCutscene()
+    {
+        cutsceneToPlay.Play();
     }
 }
