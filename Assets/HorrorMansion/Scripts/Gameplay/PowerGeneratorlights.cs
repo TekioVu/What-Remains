5a3986d2ea0ed71cebf0f00dcd2bec66c9a7f81f
Diegogog25 <diegog25@ucm.es>
2024-12-10 11:49:23 +0100

    asfsaf

---
+++ Assets/HorrorMansion/Scripts/Gameplay/PowerGeneratorlights.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerGeneratorLight : MonoBehaviour
+{
+    [SerializeField] private GameObject[] light;
+
+    public void Encenderluces() 
+    {
+        for (int i = 0; i < light.Length; i++)
+        {
+            light[i].SetActive(true);
+        }
+    } 
+
+}
