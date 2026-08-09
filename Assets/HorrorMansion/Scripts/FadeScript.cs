52c717b8faa5c315828060d46ca0bf14bfb5c558
TekioVu <sevalien@ucm.es>
2026-08-09 14:17:22 +0200

    Compatibility WARNING y transicion suave primera cinematica

--- Assets/HorrorMansion/Scripts/FadeScript.cs
+++ Assets/HorrorMansion/Scripts/FadeScript.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class FadeScript : MonoBehaviour
 {
+   [SerializeField] private bool disappear = true;
    public void Hide()
    {
+      if(disappear)
         this.gameObject.SetActive(false);
    }
 }
